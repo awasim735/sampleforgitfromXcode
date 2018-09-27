6564ca09717360a3b3d399191e22c9ac571061bb
admin <admin@Waseem-Akram-Mac.local>
2018-09-27 12:12:09 +0530

    Initial Commit

---
+++ sampleforgitfromXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleforgitfromXcode
+//
+//  Created by admin on 27/09/18.
+//  Copyright © 2018 Zee  Enterprises Entertainment Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
